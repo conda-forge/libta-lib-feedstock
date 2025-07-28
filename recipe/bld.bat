1b539ea0b01e0897a774e6f45526aa4227b31f4c
Dan Blanchard <dan.blanchard@gmail.com>
2025-07-28 18:04:45 -0400

    Copy files to right spot

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -23,3 +23,18 @@ nmake install
 if errorlevel 1 exit 1
 
 popd
+
+:: Copy files to correct locations because CMakeLists.txt overrides the it even if we set the install prefix
+:: Try both C:/Program Files (x86)/ and C:/Program Files/ to ensure we find the correct one
+for %%l in ("Program Files" "Program Files (x86)") do (
+    pushd "C:\%%l\TA-Lib"
+    copy /Y /B "lib\ta-lib.lib" "%LIBRARY_LIB%\ta-lib.lib"
+    copy /Y /B "bin\ta-lib.dll" "%LIBRARY_BIN%\ta-lib.dll"
+    copy /Y /B "lib\ta-lib-static.lib" "%LIBRARY_LIB%\ta-lib-static.lib"
+    copy /Y /B "include\ta_abstract.h" "%LIBRARY_INC%\ta_abstract.h"
+    copy /Y /B "include\ta_common.h" "%LIBRARY_INC%\ta_common.h"
+    copy /Y /B "include\ta_defs.h" "%LIBRARY_INC%\ta_defs.h"
+    copy /Y /B "include\ta_func.h" "%LIBRARY_INC%\ta_func.h"
+    copy /Y /B "include\ta_libc.h" "%LIBRARY_INC%\ta_libc.h"
+    popd
+)
